34a1c6fabc4c9d57ab9ba75dc64b59ae5d67f04e
LiShanCoder <827138050@qq.com>
2018-06-25 16:51:36 +0800

    权限验证，增加工具校验地址（支持正则匹配）

--- src/main/java/com/company/project/controller/interceptor/AuthorityInterceptor.java
+++ src/main/java/com/company/project/controller/interceptor/AuthorityInterceptor.java
@@ -1,6 +1,8 @@
 package com.company.project.controller.interceptor;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +11,6 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.company.project.po.Account;
 import com.company.project.utils.ResourcesUtil;
 
 public class AuthorityInterceptor implements HandlerInterceptor {
@@ -29,15 +30,12 @@ public class AuthorityInterceptor implements HandlerInterceptor {
 		
 		//一：匿名即可访问 (配置文件形式：anonymousURL.properties)
 		List<String> anonymousURLs = ResourcesUtil.getkeyList("urlAccess/anonymousURL");
-		for(String s : anonymousURLs) {
-			if(reqPath.equals(s))
-				return true;
-		}
+		if(isPermittedUrl(anonymousURLs, reqPath))
+			return true;
 		
 		// 不是匿名访问url，且未登陆
 		HttpSession session = request.getSession();
-		Account account = (Account)session.getAttribute("account");
-		if(session==null || account==null) {
+		if(session==null || session.getAttribute("account")==null) {
 			request.setAttribute("error", "无访问权限");
 			request.getRequestDispatcher("errPage.jsp").forward(request, response);
 			return false;								
@@ -46,20 +44,15 @@ public class AuthorityInterceptor implements HandlerInterceptor {
 		
 		//二：登陆后可访问 (配置文件形式：commonURL.properties)
 		List<String> commonURLs = ResourcesUtil.getkeyList("urlAccess/commonURL");
-		for(String s : commonURLs) {
-			if(reqPath.equals(s))
-				return true;
-		}
+		if(isPermittedUrl(commonURLs, reqPath))
+			return true;
 		
-		//三：授权后可访问 (从数据库中取出：人-角色-资源 模式)
+		//三：授权后可访问 (从数据库中取出：人-角色-资源 模式 Resource based access control)
 		//获取授权路径列表
 		@SuppressWarnings("unchecked")
-		List<String> rbac = (List<String>) session.getAttribute("urls");		//(Resource based access control)
-		//比较是否存在授权
-		for(String s: rbac) {
-			if(reqPath.equals(s))
-				return true;
-		}
+		List<String> rbac = (List<String>) session.getAttribute("urls");
+		if(isPermittedUrl(rbac, reqPath))
+			return true;
 		
 		//无权限访问当前页面，跳转到无权限提示
 		request.setAttribute("error", "无访问权限");
@@ -79,5 +72,34 @@ public class AuthorityInterceptor implements HandlerInterceptor {
 			throws Exception {
 		
 	}
-
+	
+	/**
+	 * 注意：匹配规则列表中，正则字符串必须以"regex-"打头，后接正则规则
+	 * @param checkList 匹配规则列表：全字符匹配，也可是正则匹配
+	 * @param url 验证对象
+	 * @return 验证结果
+	 */
+	private static boolean isPermittedUrl(List<String> checkList, String url) {
+		List<String> urls = new ArrayList<String>();
+		List<String> regexUrls = new ArrayList<String>();
+		//把：equals全等、正则匹配，所需String分类
+		for(String s : checkList) {
+			if(s.startsWith("regex-"))
+				regexUrls.add(s.substring(6));
+			else
+				urls.add(s);
+		}
+		//equals全等、正则匹配
+		for(String s : urls) {
+			if(s.equals(url))
+				return true;
+		}
+		for(String regex : regexUrls) {
+			if(Pattern.matches(regex, url))
+				return true;
+		}
+		
+		return false;
+	}
+	
 }
